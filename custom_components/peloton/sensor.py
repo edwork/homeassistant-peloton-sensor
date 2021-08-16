2e1fc3df0815e077fdda1b842ca21fe8248becfe
Finagain <71937166+Finagain@users.noreply.github.com>
2021-08-16 15:42:29 -0700

    API Update
    I just got my Peloton and was super excited to find this for Home Assistant!  Thank you for making this!  This is my first ever Fork/Pull Request so please forgive any errors.  I hope you don't mind the help.

--- custom_components/peloton/sensor.py
+++ custom_components/peloton/sensor.py
@@ -28,7 +28,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     }
 )
 
-
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Create the Peloton sensor."""
 
